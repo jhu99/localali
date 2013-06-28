6afbd19b92d6a2da63913d58ca91c545ad9ce146
hujialu.xd@gmail.com <hujialu.xd@gmail.com@4c31cdda-6d9c-3bde-1eea-98d106cf4aa1>
2013-06-28 13:45:24 +0000

    simulated annealing class

---
+++ trunk/src/algorithm/function.h
@@ -0,0 +1,33 @@
+/**
+Author: Jialu Hu
+Date: Jun. 11, 2013
+File name: algorithm/function.h
+Description: Searching high-scoring subnetworks.
+**/
+
+#ifndef FUNCTION_H_
+#define FUNCTION_H
+
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <random>
+#include <chrono>
+#include <deque>
+#include <stack>
+#include "verbose.h"
+#include "string.h"
+
+/// Convert number types to string type.
+template<typename TNum>
+std::string
+convert_num2str(TNum num)
+{
+	std::ostringstream buff;
+	buff<<num;
+    return buff.str();
+}
+
+#endif /// FUNCTION_H
